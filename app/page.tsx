3ba8e2b970c4de096883971f9f877a2676302adf
Anthony Lorenzo <anth.coding@gmail.com>
2024-10-31 13:48:09 -0400

    Text update

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,9 @@ export default function Page() {
       <h1 className="mb-8 text-4xl font-semibold tracking-tighter">
         Hello visitor.
       </h1>
-      <p className="mb-4 opacity-75">{`That feels distant, doesn’t it? Maybe you’re looking for someone to connect with, someone who gets it. I’m Anthony, and for me, it’s not just about code or design; it’s about creating experiences that shape how people interact with their world. It’s more than building something functional or visually appealing it’s about crafting moments that leave a lasting impression.`}</p>
-      <p className="mb-4 opacity-75">{`  If you’re here, you’re probably searching for more than just a product. You’re looking for a shared vision, a connection with someone who speaks your language. That’s where purpose meets creativity, and where great things get built together.`}</p>
+      <p className="mb-4 opacity-75">{`That feels distant, doesn’t it? Looking for someone who truly connects, who “gets it”? I’m Anthony, and to me, it’s more than code or design; it’s about crafting lasting experiences.`}</p>
+      <p className="mb-4 opacity-75">{`  My work goes beyond functionality or aesthetics—it’s about creating moments that matter. If you’re here, you’re likely looking for a shared vision. That’s where purpose meets creativity, and where great things get built together.
+`}</p>
       <div className="my-8">
         <h3 className="text-xl font-semibold">Pinned Projects</h3>
         <Projects />
